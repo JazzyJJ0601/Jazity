d854e9e58b20599eae77161c5df612a4cf3c58b8
w3dawd <jasperjensen8888@gmail.com>
2024-09-15 18:10:27 +0100

    forget to end comment :0

--- Engine/Core/Main.cpp
+++ Engine/Core/Main.cpp
@@ -5,6 +5,7 @@
  * 3 Set include and libary directories
  * 4 Set force include file (GameEntity.h)
  * 5 Set c++ language standard to c++17 and calling convention to __fastcall
+ */
 
 int main()
 {
